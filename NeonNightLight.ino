cead5c5dc07ec8b347a2d0aca09ee67125e3d0ee
RandyHoo <ryozero1996@yahoo.co.jp>
2016-11-27 12:06:26 +0900

    完成版コードをアップ
    完成版コードをアップロード

---
+++ NeonNightLight.ino
@@ -0,0 +1,25 @@
+#define SWInputPin 6 //動作モード用SW
+#define NeonOutputPin 4 //ネオン管点灯用ピン
+#define CdsInputPin 5 //Cdsセル入力ピン
+
+void setup() {
+  pinMode(SWInputPin, INPUT);
+  pinMode(NeonOutputPin, OUTPUT);
+}
+
+void loop(){
+
+  if(LOW == digitalRead(SWInputPin)){
+    //暗い時のみ
+    if(analogRead(CdsInputPin) >= 900){
+      digitalWrite(NeonOutputPin, HIGH);
+    }else{
+      digitalWrite(NeonOutputPin, LOW);
+    }
+  }
+  //常時点灯
+  else{
+    digitalWrite(NeonOutputPin, HIGH);
+  }
+
+}
